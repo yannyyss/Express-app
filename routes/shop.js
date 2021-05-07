de2e166be3ac50f2dfa23e9c2ad07dc5c790a2f3
Yanely <yannyyss@hotmail.com>
2021-05-06 22:25:20 -0500

    path function added to routes files

--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const path = require('path')
+const routeDirectory = require('../utils/path')
 
 const router = express.Router()
 
 router.get('/',(req, res) => { // It will executed just when "/" match exactly because of the get (if we use "use", match with "/---whatever")
-    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html')) // __dirname is a global variable whic hold the absolutely path 
+    res.sendFile(path.join(routeDirectory, 'views', 'shop.html')) // __dirname is a global variable whic hold the absolutely path 
 })
 
 module.exports = router
---
+++ utils/path.js
@@ -0,0 +1,3 @@
+const path = require('path')
+
+module.exports = path.dirname(require.main.filename) // .dirname returns the directory name of the path
